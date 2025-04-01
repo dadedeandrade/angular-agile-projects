201ac33ebce0055a52618fa0397f0e87c9bd1568
dadedeandrade <viniciusdandrade01@gmail.com>
2025-03-31 21:13:43 -0300

    feat/ move toolbar logic to App so it can interact with router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-agile-projects';
+  constructor(private dialog: MatDialog) {}
+
+  openAddDialogProject() {
+    this.openDialog(DialogComponent);
+  }
+
+  openDialog(component: any) {
+    let dialog = this.dialog.open(component);
+    dialog.afterClosed().subscribe((item) => {
+      console.log('Dados afterclose', item);
+    });
+  }
 }
